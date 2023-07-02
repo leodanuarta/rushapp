714a16700d4da8410c40bf4542f7614cbf4e9633
leodanuarta <leo.535210024@stu.untar.ac.id>
2023-07-02 15:22:17 +0700

    fixed, sudah jadi

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,7 @@
 import { doc, onSnapshot } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 import { db } from '../firebase';
 
 const Chats = () => {
@@ -8,6 +9,7 @@ const Chats = () => {
   const [chats, setChats] = useState([]);
 
   const {currentUser} = useContext(AuthContext);
+  const {dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -26,15 +28,19 @@ const Chats = () => {
 
   console.log(Object.entries(chats));
 
+  const handleSelect = (u) => {
+    dispatch({type: "CHANGE_USER", payload: u});
+  };
+
   return (
     <div className="chats">
-      {Object.entries(chats)?.map((chat) => (
+      {Object.entries(chats)?.sort((a,b) => b[1].date - a[1].date).map((chat) => (
 
-        <div className="userChat" key={chat[0]}>
+        <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)} >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].userInfo.lastMessage?.text} World</p>
+            <p>{chat[1].lastMessage?.text}</p>
           </div>
         </div>
       ))}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,18 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { auth, db, storage } from '../firebase';
-import Add from '../img/addAvatar.png';
+import { auth, db, storage } from "../firebase";
+import Add from "../img/addAvatar.png";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
@@ -18,65 +20,69 @@ const Register = () => {
     const file = e.target[3].files[0];
 
     try {
+      //Create user
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, displayName);
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      // Register three observers:
-      uploadTask.on(
-        (error) => {
-          // Handle unsuccessful uploads
-          setErr(true);
-        },
-        
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
             });
-            await setDoc(doc(db, "users", res.user.uid),{
-              uid : res.user.uid,
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
               displayName,
               email,
-              photoURL : downloadURL
+              photoURL: downloadURL,
             });
-            await setDoc(doc(db, "userChats", res.user.uid),{
 
-            });
-            navigate('/');
-          });
-        }
-      );
-    } catch(err){
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
       setErr(true);
+      setLoading(false);
     }
   };
-
 
   return (
     <div className="formContainer">
       <div className="formWrapper">
-       <span className="logo">RushApp</span>
-       <span className="title">Register</span>
+        <span className="logo">RushApp</span>
+        <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Display Name' />
-          <input type="email"placeholder='Email'  />
-          <input type="password" placeholder='Password'  />
-          <input style={{display: 'none'}} type="file"id="file" />
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
             <img src={Add} alt="" />
-            <span>Add an Avatar</span>
+            <span>Add an avatar</span>
           </label>
-          <button>Sign Up</button>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
           {err && <span>Something went wrong</span>}
         </form>
-        <p>You do have an account ? <Link to='/login'>Login</Link> </p>
+        <p>
+          You do have an account? <Link to="/register">Login</Link>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
