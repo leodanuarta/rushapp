fb60da877e49c6202a33d6068409e4cc86773e89
leodanuarta <leo.535210024@stu.untar.ac.id>
2023-07-10 19:38:24 +0700

    fix .env bug

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-require('dotenv').config()
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
